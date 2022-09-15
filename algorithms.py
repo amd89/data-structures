ab68a4a0d1b1fc11f9a67cc1b157bfdca1f16905
Andre Dallaire <adallaire@live.com>
2022-09-15 09:53:36 -0400

    Add files via upload

---
+++ algorithms.py
@@ -0,0 +1,37 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+def bubble_sort(my_list):
+    for i in range(len(my_list) - 1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j + 1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = temp
+    return my_list
+
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        min_index = i
+        for j in range(i + 1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if min_index != i:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
+    return my_list
+
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i - 1
+        while temp < my_list[j] and j > -1:
+            my_list[j + 1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+print(insertion_sort([4,2,6,5,1,3]))
