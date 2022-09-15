b9111f26206f84519947b2e61cf92112d0e637cf
Andre Dallaire <adallaire@live.com>
2022-09-15 12:30:59 -0400

    Add files via upload

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -1,3 +1,6 @@
+from unittest import result
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -45,18 +48,87 @@ class BinarySearchTree:
             current_node = current_node.left
         return current_node
 
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+
+    def dfs_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
         
+        traverse(self.root)
+        return results
 
+    def dfs_post_order(self):
+        results = []
+        
+        def traverse(current_node):
+            
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+
+        traverse(self.root)
+        return results
+
+    def dfs_in_order(self):
+        results = []
+        
+        def traverse(current_node):
+            
+            if current_node.left is not None:
+                traverse(current_node.left)
+
+            results.append(current_node.value)
+
+            if current_node.right is not None:
+                traverse(current_node.right)
+            
+        traverse(self.root)
+        return results
+
+
+# my_tree = BinarySearchTree()
+# my_tree.insert(2)
+# my_tree.insert(1)
+# my_tree.insert(3)
+
+# print(my_tree.min_value(my_tree.root).value)
+
+# print(my_tree.contains(2))
+# print(my_tree.contains(5))
+
+# print(my_tree.root.value)
+# print(my_tree.root.left.value)
+# print(my_tree.root.right.value)
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
-
-print(my_tree.min_value(my_tree.root).value)
-
-print(my_tree.contains(2))
-print(my_tree.contains(5))
-
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+print(my_tree.BFS())
+print(my_tree.dfs_pre_order())
+print(my_tree.dfs_post_order())
+print(my_tree.dfs_in_order())
